a38f4c3d03621d7811381200a55199a0146435e5
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-17 19:05:30 +0200

    adding applicatino header component

---
+++ src/components/ApplicationHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-row w-full justify-between items-center">
+        <div>
+            <img :width="300" src="https://interworks.cloud/wp-content/uploads/2022/08/interworks.cloud-logo.svg" />
+            <p>Assignment authored by Konstantinos Tsirakos</p>
+        </div>
+        <ShoppingCartIconButton />
+    </div>
+</template>
+
+<script setup lang="ts">
+import ShoppingCartIconButton from './ShoppingCartIconButton.vue';
+</script>
