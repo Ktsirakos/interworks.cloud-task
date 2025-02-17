c8dab232b965676948d69296cfa6ac77877b9d94
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-17 19:05:16 +0200

    adding components - notification badge wrapper - shopping cart button

---
+++ src/components/NotificationBadge.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="relative inline-block">
+        <span class="
+        absolute 
+        -top-2
+        -right-2
+        bg-red-500 
+        rounded-4xl 
+        w-6 h-6
+        text-lg
+        flex 
+        items-center
+        justify-center
+        z-10
+        ">{{ props.count }}</span>
+
+        <div class="relative">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    count: number
+}>()
+</script>
---
+++ src/components/ShoppingCartIconButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <NotificationBadge :count="2">
+        <ShoppingCart class="hover:text-cyan-500 cursor-pointer" :size="42" />
+    </NotificationBadge>
+</template>
+
+<script setup lang="ts">
+import { ShoppingCart } from 'lucide-vue-next';
+import NotificationBadge from './NotificationBadge.vue';
+
+</script>
