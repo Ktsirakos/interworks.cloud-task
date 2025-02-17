0dfb0cce1fe7dce0ddf64708dc56e15f283f52d1
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-17 18:16:42 +0200

    initial project modifications - removing boilerplate components - adding tailwind css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p class="text-yellow-500 text-5xl">Test</p>
   </main>
 </template>
