9d2646e896aa3f6bbc844baff79443dbae0be5b4
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-18 15:52:58 +0200

    making drawer scrollable and adding basket item

--- src/components/BasketDrawer.vue
+++ src/components/BasketDrawer.vue
@@ -1,9 +1,13 @@
 <template>
-    <div :class="`absolute transition-all duration-500 ${props.open ? 'w-3/10' : 'w-0'} bg-white h-full right-0 z-10`">
-        <div class="py-10 px-5">
+    <div
+        :class="`fixed transition-all duration-500 ${props.open ? 'w-3/10' : 'w-0'} bg-stone-500 h-full right-0 z-100 ${props.open ? 'overflow-y-auto' : 'overflow-hidden'}`">
+        <div v-if="props.open" class="py-10 px-5">
             <div class="flex flex-row justify-between items-center">
-                <p class="text-black text-5xl">Basket</p>
-                <X @click="() => basketStore.closeBasketDrawer()" :size="52" color="black" class="cursor-pointer" />
+                <p class="text-5xl">Basket</p>
+                <X @click="() => basketStore.closeBasketDrawer()" :size="52" color="white" class="cursor-pointer" />
+            </div>
+            <div class="py-10 gap-4">
+                <BasketItem v-for="product of basketStore.basketItems" :product="product.item" :key="product.item.id" />
             </div>
         </div>
     </div>
@@ -12,9 +16,29 @@
 <script setup lang="ts">
 import { useBasketStore } from '@/stores/basket';
 import { X } from 'lucide-vue-next';
+import BasketItem from './BasketItem.vue';
+import { watch, onMounted, onUnmounted } from 'vue';
 const props = defineProps<{
     open: boolean
 }>()
 
 const basketStore = useBasketStore()
+
+watch(() => props.open, (newValue) => {
+    if (newValue) {
+        document.body.style.overflow = 'hidden'; //disabling body scroll
+    } else {
+        document.body.style.overflow = ''; //enabling body scroll
+    }
+});
+
+onMounted(() => {
+    if (props.open) {
+        document.body.style.overflow = 'hidden'; //disabling body scroll    
+    }
+});
+
+onUnmounted(() => {
+    document.body.style.overflow = ''; //enabling body scroll
+});
 </script>
---
+++ src/components/BasketItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-row gap-2 overflow-hidden my-5">
+        <img class="w-1/4" :src="props.product.image" />
+        <div>
+            <p class="text-2xl">{{ props.product.name }}</p>
+            <CurrencyText locale="EU" class="text-2xl" :value="props.product.price" />
+        </div>
+    </div>
+    <ProductCardActions :item="product" />
+</template>
+
+<script setup lang="ts">
+import type { Product } from "@/api/types";
+import CurrencyText from "./CurrencyText.vue";
+import ProductCardActions from "./ProductCardActions.vue";
+
+const props = defineProps<{
+    product: Product
+}>()
+</script>
