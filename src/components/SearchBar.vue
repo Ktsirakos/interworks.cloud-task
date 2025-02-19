457abecdebafd00b07e539179ab4df781dceb67f
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-19 12:50:40 +0200

    adding search functionality

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <input placeholder="Search products" class="border-2 rounded-xl p-5 text-xl w-full" v-model="value" />
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const value = ref<string>()
+const emits = defineEmits(['onQueryChange'])
+
+watch(value, (newValue) => {
+    emits('onQueryChange', newValue)
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,26 @@
 import { ApiClient } from '@/api/client';
 import type { Product } from '@/api/types';
 import ProductCard from '@/components/ProductCard.vue';
+import SearchBar from '@/components/SearchBar.vue';
 import { ref } from 'vue';
 
 const items = ref<Product[]>()
+const initialItems = ref<Product[]>([])
 ApiClient.fetchAllProducts().then((value) => {
   items.value = value
+  initialItems.value = value
 })
+
+const filterProducts = (query: string) => {
+  console.log(query)
+  items.value = initialItems.value?.filter(e => e.name.includes(query))
+}
 
 </script>
 
 <template>
-  <main class="py-10">
+  <main class="pb-10 flex flex-col gap-5">
+    <SearchBar @on-query-change="filterProducts" />
     <div class="grid grid-cols-3 gap-10">
       <ProductCard v-for="item of items" :key="item.name" :product="item" />
     </div>
